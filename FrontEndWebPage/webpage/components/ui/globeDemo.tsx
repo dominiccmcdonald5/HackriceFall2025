b815eaf9c925bc1ae28efe90689aff23bd6825c7
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-09-20 18:38:06 -0500

    Added new Design of Website

--- FrontEndWebPage/webpage/components/ui/globeDemo.tsx
+++ FrontEndWebPage/webpage/components/ui/globeDemo.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { Globe } from "@/components/ui/globe";
 
 export default function GlobeDemo() {
   return (
-    <div className="relative flex w-full max-w-lg items-center justify-center rounded-lg border bg-background p-8">
-      <Globe />
-    </div>
+  <div className="relative flex w-full max-w-lg items-center justify-center rounded-lg p-8 bg-white/10">    
+  <Globe />
+  </div>
+
+
   );
 }
