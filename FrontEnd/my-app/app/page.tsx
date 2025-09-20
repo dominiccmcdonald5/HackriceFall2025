dc5eabce284a4c075934242f6cae6134761f1ed7
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-09-20 01:00:14 -0500

    Added Globe

---
+++ FrontEnd/my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import GlobeClient from "@/components/GlobeClient";
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+  <GlobeClient />
+      </main>
+  );
+}
