03af45b256159e73b83cd75439c7e7a091c426eb
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-09-21 03:40:22 -0500

    Fixing

--- FrontEndWebPage/webpage/components/globeClient.tsx
+++ FrontEndWebPage/webpage/components/globeClient.tsx
@@ -3,12 +3,12 @@
 import dynamic from "next/dynamic";
 import type { MyMarker } from "@/components/ui/globe";
 
-const Globe = dynamic(() => import("@/components/ui/globe"), { ssr: false });
+const GlobeDemo = dynamic(() => import("@/components/ui/globe"), { ssr: false });
 
 interface GlobeClientProps {
   onMarkerClick: (marker: MyMarker) => void;
 }
 
 export default function GlobeClient({ onMarkerClick }: GlobeClientProps) {
-  return <Globe onMarkerClick={onMarkerClick} />;
+  return <GlobeDemo onMarkerClick={onMarkerClick} />;
 }
